c0637dbb1465935836da820030390986551ec620
LikinaJothiS <likinajothi.s@thoughtworks.com>
2022-02-14 17:28:38 +0530

    Likina | Request mapping for the end point

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-
+@RequestMapping("/")
 public class WelcomeController {
     @GetMapping("/welcome")
     public String HelloWorld(){
